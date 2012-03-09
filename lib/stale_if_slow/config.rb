61279417fe6e827bde05b5cb81c59ec6a2b16791
tulios <ornelas.tulio@gmail.com>
2012-03-09 00:14:31 -0300

    improvements on config object allowing [] access to the options

---
+++ lib/stale_if_slow/config.rb
@@ -0,0 +1,33 @@
+module StaleIfSlow
+  class Config
+    
+    attr_reader :options
+    
+    def initialize
+      @options = {
+        cache_store: ActiveSupport::Cache.lookup_store(:memory_store),
+        logger: Logger.new(STDOUT),
+        logger_level: Logger::INFO,
+        timeout: 0.3, # 300 milliseconds
+        content_timeout: 5.minutes,
+        stale_content_timeout: 30.minutes
+      }      
+    end
+    
+    def apply! &block
+      instance_exec(&block) if block_given?
+      options[:logger].level = options[:logger_level]
+    end
+      
+    [:cache_store, :logger, :logger_level, :timeout, :content_timeout, :stale_content_timeout].each do |key|
+      define_method(key.to_s) do |value|
+        @options[key] = value
+      end
+    end
+    
+    def [] key
+      options[key]
+    end
+            
+  end 
+end
