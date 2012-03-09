ffaf83d5e14b321193cda3509506ad781df2d2b1
tulios <ornelas.tulio@gmail.com>
2012-03-09 00:15:53 -0300

    small fix for configure method; require of other files

--- lib/stale_if_slow.rb
+++ lib/stale_if_slow.rb
@@ -1,6 +1,31 @@
+require "logger"
 require "timeout"
-require "stale_if_slow/version"
+require 'digest/md5'
+require "active_support"
+
+require_relative "stale_if_slow/version"
+require_relative "stale_if_slow/config"
+require_relative "stale_if_slow/api"
+require_relative "stale_if_slow/timeout_performer"
+require_relative "stale_if_slow/key_generator"
 
 module StaleIfSlow
-  # Your code goes here...
+  class Error < StandardError; end
+  
+  class << self
+    
+    def config
+      @@config ||= Config.new
+    end
+    
+    def configure &block
+      config.apply!(&block)
+      true
+    end
+    
+    def log type, message
+      config[:logger].send(type, "[StaleIfSlow] :: #{message}")
+    end
+    
+  end  
 end
