8fbc63071756198c52b8d22297e9a5d018bbe8f0
tulios <ornelas.tulio@gmail.com>
2012-03-12 21:55:15 -0300

    pending tests

--- spec/stale_if_slow/timeout_performer_spec.rb
+++ spec/stale_if_slow/timeout_performer_spec.rb
@@ -11,61 +11,61 @@ describe StaleIfSlow::TimeoutPerformer do
   
   describe "when initializing" do
     it "should keep a copy of reference" do
-      
+      pending
     end
     
     it "should keep a copy of method" do
-      
+      pending      
     end
     
     it "should keep a copy of the original implementation of method" do
-      
+      pending
     end
     
     it "should create a new instance of KeyGenerator" do
-      
+      pending
     end
     
     it "should retrieve cache_store configuration" do
-      
+      pending
     end
     
     it "should retrieve timeout configuration" do
-      
+      pending
     end
     
     it "should retrieve content_timeout configuration" do
-      
+      pending
     end
     
     it "should retrieve stale_content_timeout configuration" do
-      
+      pending
     end
   end
   
   describe "when execute method call" do
     it "should return cached content if cache is still hot" do
-      
+      pending
     end
     
     describe "and cache already expired" do
       describe "and have cached content" do
         it "should wait the configured timeout and refresh the cache" do
-          
+          pending
         end
         
         it "should return stale content if a StaleIfSlow::Error occur" do
-          
+          pending
         end
         
         it "should propagate the exception in case of error" do
-          
+          pending
         end
       end
       
       describe "and does not have cached content" do
         it "should complete the call" do
-          
+          pending
         end
       end
     end
