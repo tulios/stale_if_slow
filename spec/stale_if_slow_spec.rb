e503aa26a13837ef3d9f95079a885b8a96fe8d36
tulios <ornelas.tulio@gmail.com>
2012-03-16 20:13:31 -0300

    some tests for the main module

--- spec/stale_if_slow_spec.rb
+++ spec/stale_if_slow_spec.rb
@@ -24,8 +24,34 @@ describe StaleIfSlow do
   end
   
   describe "#configure" do
+    subject do
+      StaleIfSlow.config
+    end
+      
+    let! :store do
+      ActiveSupport::Cache.lookup_store(:memory_store)
+    end
+  
+    before do
+      ActiveSupport::Cache.stub(:lookup_store).with(:memory_store).and_return(store)
+    end
+      
     it "should set the configurations on config object" do
-      pending
+      StaleIfSlow.configure do
+        cache_store ActiveSupport::Cache.lookup_store(:memory_store)
+        logger Logger.new(STDOUT)
+        logger_level Logger::WARN
+        timeout 0.4
+        content_timeout 10.minutes
+        stale_content_timeout 1.hour
+      end
+      
+      subject[:cache_store].should eql store
+      subject[:logger].should be_instance_of Logger
+      subject[:logger_level].should eql Logger::WARN
+      subject[:timeout].should eql 0.4
+      subject[:content_timeout].to_i.should eql 10.minutes.to_i
+      subject[:stale_content_timeout].to_i.should eql 1.hour.to_i
     end
   end
 end
